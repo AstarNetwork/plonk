c450e099490911843752006b3269269febb4c8d0
NoCtrlZ <phantomofrotten@gmail.com>
2021-07-08 18:53:14 +0900

    depulicate srs to rpc

--- pallets/template/rpc/src/lib.rs
+++ pallets/template/rpc/src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+mod srs;
+
+// #[rpc]
+// pub trait PlonkStorageApi {
+//     #[rpc(name = "plonk_get_srs")]
+//     fn get_srs(&self) -> Result<
+// }
