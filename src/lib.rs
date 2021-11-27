7f9305ef776ac4f4aa1c6eb1c4a59675476db46c
NoCtrlZ <phantomofrotten@gmail.com>
2021-11-27 11:32:31 +0900

    change condition

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ mod tests;
 #[frame_support::pallet]
 pub mod pallet {
     use dusk_plonk::prelude::*;
+    use frame_support::dispatch::{DispatchErrorWithPostInfo, PostDispatchInfo};
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
     use rand_core::OsRng;
@@ -41,11 +42,20 @@ pub mod pallet {
         #[pallet::weight(10_000)]
         pub fn trusted_setup(origin: OriginFor<T>, val: u32) -> DispatchResultWithPostInfo {
             let _ = ensure_signed(origin)?;
-            let pp = PublicParameters::setup(1 << val, &mut OsRng).unwrap();
-            PublicParameter::<T>::put(&pp);
-
-            Event::<T>::TrustedSetup(pp);
-            Ok(().into())
+            match Self::public_parameter() {
+                Some(_) => {
+                    return Err(DispatchErrorWithPostInfo {
+                        post_info: PostDispatchInfo::from(()),
+                        error: DispatchError::Other("Already setup"),
+                    })
+                }
+                None => {
+                    let pp = PublicParameters::setup(1 << val, &mut OsRng).unwrap();
+                    PublicParameter::<T>::put(&pp);
+                    Event::<T>::TrustedSetup(pp);
+                    return Ok(().into());
+                }
+            }
         }
 
         #[pallet::weight(10_000)]
@@ -57,10 +67,19 @@ pub mod pallet {
             _transcript_init: Vec<u8>,
         ) -> DispatchResultWithPostInfo {
             let _ = ensure_signed(origin)?;
-            let pp = Self::public_parameter().unwrap();
-            T::CustomCircuit::verify(&pp, &vd, &proof, &public_inputs, b"Test")
-                .expect("verify process is in valid");
-            Ok(().into())
+            match Self::public_parameter() {
+                Some(pp) => {
+                    T::CustomCircuit::verify(&pp, &vd, &proof, &public_inputs, b"Test")
+                        .expect("verify process is in valid");
+                    Ok(().into())
+                }
+                None => {
+                    return Err(DispatchErrorWithPostInfo {
+                        post_info: PostDispatchInfo::from(()),
+                        error: DispatchError::Other("Setup not yet"),
+                    })
+                }
+            }
         }
     }
 }
