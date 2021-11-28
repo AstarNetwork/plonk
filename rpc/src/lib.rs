7ac65b426751095c32ed969fa512a468a21376fb
NoCtrlZ <phantomofrotten@gmail.com>
2021-11-29 08:37:01 +0900

    add runtime trait

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -1,8 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use dusk_plonk::prelude::PublicParameters;
+use jsonrpc_core::{Error as RpcError, ErrorCode, Result};
+use jsonrpc_derive::rpc;
+use plonk_runtime_api::PlonkApi as PlonkRuntimeApi;
+use sp_api::ProvideRuntimeApi;
+use sp_blockchain::HeaderBackend;
+use sp_runtime::{generic::BlockId, traits::Block as BlockT};
+use std::sync::Arc;
+
+#[rpc]
+pub trait PlonkApi<BlockHash> {
+    #[rpc(name = "plonk_getPublicParameters")]
+    fn get_public_parameters(&self, at: Option<BlockHash>) -> Result<PublicParameters>;
+}
+
+pub struct Plonk<C, M> {
+    client: Arc<C>,
+    _marker: std::marker::PhantomData<M>,
+}
+
+impl<C, M> Plonk<C, M> {
+    pub fn new(client: Arc<C>) -> Self {
+        Self {
+            client,
+            _marker: Default::default(),
+        }
     }
 }
