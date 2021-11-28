92ec37000004f14546cb0766e1c1c8229be9b817
NoCtrlZ <phantomofrotten@gmail.com>
2021-11-29 08:53:29 +0900

    define rpc interface

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -26,3 +26,32 @@ impl<C, M> Plonk<C, M> {
         }
     }
 }
+
+impl<C, Block> PlonkApi<<Block as BlockT>::Hash> for Plonk<C, Block>
+where
+    Block: BlockT,
+    C: Send + Sync + 'static,
+    C: ProvideRuntimeApi<Block>,
+    C: HeaderBackend<Block>,
+    C::Api: PlonkRuntimeApi<Block>,
+{
+    fn get_public_parameters(
+        &self,
+        at: Option<<Block as BlockT>::Hash>,
+    ) -> Result<PublicParameters> {
+        let api = self.client.runtime_api();
+        let at = BlockId::hash(at.unwrap_or_else(|| self.client.info().best_hash));
+
+        let runtime_api_result = api.get_public_parameters(&at);
+        match runtime_api_result {
+            Ok(i) => return Ok(i.unwrap()),
+            Err(e) => {
+                return Err(RpcError {
+                    code: ErrorCode::ServerError(9876),
+                    message: "Something wrong".into(),
+                    data: Some(format!("{:?}", e).into()),
+                })
+            }
+        }
+    }
+}
