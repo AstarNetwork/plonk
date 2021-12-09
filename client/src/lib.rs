bf110d44d1949b8f100061db16446e0b582a54d7
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-09 09:23:49 +0900

    get public parameters

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,8 +1,47 @@
+#![allow(dead_code)]
+
+use futures::Future;
+use jsonrpc_client_transports::*;
+
+#[derive(Clone)]
+struct NodeClient(TypedClient);
+
+impl From<RpcChannel> for NodeClient {
+    fn from(channel: RpcChannel) -> Self {
+        NodeClient(channel.into())
+    }
+}
+
+impl NodeClient {
+    fn get_public_parameters(&self) -> impl Future<Item = String, Error = RpcError> {
+        self.0.call_method("plonk_getPublicParameters", "[]", ())
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use hyper::rt;
+    use jsonrpc_client_transports::transports::http::connect;
+    use std::time::Duration;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_get_public_parameters() {
+        let endpoint = "http://localhost:9933";
+        let (tx, rx) = std::sync::mpsc::channel();
+        let run = connect(&endpoint)
+            .and_then(|client: NodeClient| {
+                client.get_public_parameters().and_then(move |result| {
+                    drop(client);
+                    let _ = tx.send(result);
+                    Ok(())
+                })
+            })
+            .map_err(|e| println!("RPC Client error: {:?}", e));
+
+        rt::run(run);
+
+        let result = rx.recv_timeout(Duration::from_secs(3)).unwrap();
+        println!("{:?}", result);
     }
 }
